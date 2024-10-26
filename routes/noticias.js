0af9d19d4a39a0ce732713b2ab28ddf9987b4a54
Kevin Niño <kevinandresrg@gmail.com>
2024-10-26 18:37:13 -0500

    primer commit

---
+++ routes/noticias.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+
+router.get("/", async (req, res) => {
+  const { categoria, buscar } = req.query;
+  try {
+    // aqui va las apis de noticias
+  } catch (error) {}
+});
